77a32740f0318c4df6adca6beedbdc6640473f98
Amr Abdelghany Abdou <amr.a.abdou@stud.h-da.de>
2021-08-30 21:55:28 +0200

    Docker-compose added  MySQL Added  Basic Layout for backend added  Env Configuration added

---
+++ backend/src/index.ts
@@ -0,0 +1,35 @@
+// tslint:disable-next-line: no-var-requires
+require('dotenv-safe').config();
+var cors = require('cors');
+import express, { Request, Response } from 'express';
+import { createDatabaseConnection } from './util/createDatabaseConnection';
+import * as bodyParser from 'body-parser';
+
+import morgan from 'morgan';
+
+const port: number = Number(process.env.PORT);
+
+export const startServer = async () => {
+  try {
+    var corsOptions = {
+      origin: "*"
+    }
+    const app = express();
+    const dbConnection = await createDatabaseConnection();
+
+    app.use(bodyParser.json());
+    app.use(morgan('combined'));
+    app.get('/', (req, res) => {
+      res.send("Hello World");
+    })
+
+    const server = app.listen(port, () => console.log(`Server is running on port ${port}`));
+    return { server, dbConnection };
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+// tslint:disable-next-line: no-floating-promises
+startServer();
