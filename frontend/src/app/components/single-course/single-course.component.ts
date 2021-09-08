84da23d5eaac4ed29da29f69b60954924cbef137
Amr Abdelghany Abdou <amr.a.abdou@stud.h-da.de>
2021-09-08 17:35:17 +0200

    final commit

--- frontend/src/app/components/single-course/single-course.component.ts
+++ frontend/src/app/components/single-course/single-course.component.ts
@@ -11,61 +11,66 @@ import { MainComponent } from '../main/main.component';
 })
 export class SingleCourseComponent implements OnInit {
 
-  pathUrl: string="http://www.archeonavale.org/pdf/cordeliere/test.pdf";
+  pathUrl: string = "http://www.archeonavale.org/pdf/cordeliere/test.pdf";
   materials
 
   constructor(private route: ActivatedRoute, private httpClient: HttpClient,
     private dialogService: NbDialogService) { }
 
-    pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+  pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
 
   ngOnInit(): void {
     let token = localStorage.getItem('token');
     const requestOptions = {
-      headers: new HttpHeaders({'Authorization': token ? JSON.parse(token) : "no token found"})
+      headers: new HttpHeaders({ 'Authorization': token ? JSON.parse(token) : "no token found" })
     };
-      this.httpClient.get<any>(`http://localhost:3000/api/courses/${this.route.snapshot.params.id}`, requestOptions)
+    this.httpClient.get<any>(`http://localhost:3000/api/courses/${this.route.snapshot.params.id}`, requestOptions)
       .subscribe(
         response => {
-          this.materials=response.materials;
+          this.materials = response.materials;
         },
         error => {
         }
       )
 
   }
-    getPDF(data) {
-    return  'data:application/pdf;base64,'+data;
+  getPDF(data) {
+    return 'data:application/pdf;base64,' + data;
   }
-   open(dialog: TemplateRef<any>, idMatriel) {
-     let token = localStorage.getItem('token');
-     const requestOptions = {
-       headers: new HttpHeaders({'Authorization': token ? JSON.parse(token) : "no token found"})
-     };
-     this.httpClient.get<any>(`http://localhost:3000/api/materialContents/${idMatriel}`, requestOptions)
-       .subscribe(
-         response => {
-           this.pathUrl = response.data.content.data;
+  open(dialog: TemplateRef<any>, idMatriel) {
+    let token = localStorage.getItem('token');
+    const requestOptions = {
+      headers: new HttpHeaders({
+         'Authorization': token ? JSON.parse(token) : "no token found",
+         'responseType'  : 'arraybuffer' as 'json'
+        })
+    };
+    this.httpClient.get<any>(`http://localhost:3000/api/materialContents/${idMatriel}`, requestOptions)
+      .subscribe(
+        response => {
+          let file = new Blob([response.data.content.data], { type: 'application/pdf' });
+          this.pathUrl = window.URL.createObjectURL(file);
+          // window.open(this.pathUrl);
 
-           //this.pathUrl = response.data.content.data;
-           //this.pathUrl = this.getPDF( response.data.content.data)
+          //this.pathUrl = response.data.content.data;
+          //this.pathUrl = this.getPDF( response.data.content.data)
 
-           //this.pdfSrc = response.data.content.data
-         },
-         error => {
-         }
-       )
+          //this.pdfSrc = response.data.content.data
+        },
+        error => {
+        }
+      )
 
     this.dialogService.open(dialog, { context: 'this is some additional data passed to dialog' });
     //this.pathUrl =  'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
   }
 
-  change(path){
-   var ifrm = document.createElement("iframe");
-        ifrm.setAttribute("src", path);
-        ifrm.style.width = "640px";
-        ifrm.style.height = "480px";
-        document.body.appendChild(ifrm);
+  change(path) {
+    var ifrm = document.createElement("iframe");
+    ifrm.setAttribute("src", path);
+    ifrm.style.width = "640px";
+    ifrm.style.height = "480px";
+    document.body.appendChild(ifrm);
   }
 
 }
